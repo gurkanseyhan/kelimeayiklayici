4095e3a86245b45f6e547d4465d1f5e6568d0ae7
Gürkan <gurkan.seyhan@gmail.com>
2018-11-14 13:07:31 +0300

    büyük harf küçültücü
    Küçük büyük harf den kaynaklı farklı kelime algılama sorunu için metnin büyük harflerini küçüğe dönüştürecek mekanizma eklendi

--- kelimeayiklayici.py
+++ kelimeayiklayici.py
@@ -1,6 +1,22 @@
-# coding=utf-8
+# -*- coding: utf8 -*-
+import sys
 import docx
 from collections import Counter
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+alphabet = {
+    'A': 'a', 'B': 'b', 'C': 'c',
+    'Ç': 'ç', 'D': 'd', 'E': 'e',
+    'F': 'f', 'G': 'g', 'Ğ': 'ğ',
+    'H': 'h', 'I': 'ı', 'İ': 'i',
+    'J': 'j', 'K': 'k', 'L': 'l',
+    'M': 'm', 'N': 'n', 'O': 'o',
+    'Ö': 'ö', 'P': 'p', 'R': 'r',
+    'Ş': 'ş', 'S': 's', 'T': 't',
+    'U': 'u', 'Ü': 'ü', 'V': 'v',
+    'Y': 'y', 'Z': 'z'
+}
 
 
 replace_list = ['.', ',', '?',
@@ -14,11 +30,14 @@ def get_key_piece(filename):
     document = ""
     tem_document = docx.Document(filename)
     for _par in tem_document.paragraphs:
-        document += _par.text
+        document = document + " " + _par.text
     clear_punctuation(document)
 
 
 def clear_punctuation(doc_text):
+    for _test in alphabet.keys():
+        doc_text = doc_text.replace(_test, alphabet[_test])
+
     for _rep in replace_list:
         doc_text = doc_text.replace(_rep, " ")
     return clear_repeat(doc_text.split(" "))
@@ -27,10 +46,11 @@ def clear_punctuation(doc_text):
 def clear_repeat(punc_clear_list):
     a = Counter(punc_clear_list)
     for _aa in a.keys():
-        if _aa.strip() != "":
-            print(_aa + " : " + str(a[_aa]))
+        if _aa.strip() != "" and _aa.strip != " ":
+            print(_aa.strip() + " : " + str(a[_aa]))
 
     print ("Farklı Kelime Sayısı : " + str(len(a)))
 
 
-get_key_piece("ornekmetin.docx")
+filename = raw_input("Lütfen dosya adını uzantısı ile beraber yazınız : ")
+get_key_piece(filename)
